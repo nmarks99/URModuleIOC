49f9056a8bcb8cd3bf9f93bf8b462ca56509e76d
nmarks99 <nmarks@anl.gov>
2025-05-29 15:09:44 -0500

    initial commit

---
+++ iocBoot/iocxxx/st.cmd
@@ -0,0 +1,21 @@
+# ../../bin/${EPICS_HOST_ARCH}/xxx st.cmd
+< envPaths
+
+dbLoadDatabase("../../dbd/iocxxxLinux.dbd")
+iocxxxLinux_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet("IOCSH_PS1", "$(IOC)>")
+epicsEnvSet("PREFIX", "xxx:")
+
+# Instantiate the client with a name
+AsynHttpClientConfig("client1")
+
+# Example: Control robot arm with REST API
+iocshLoad("ur_module.iocsh", "PORT=client1")
+
+###############################################################################
+iocInit
+###############################################################################
+
+# print the time our boot was finished
+date
