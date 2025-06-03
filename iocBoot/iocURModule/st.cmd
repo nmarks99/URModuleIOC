377656660d3379e26b2d8233da0a31473fad9d71
nmarks99 <nmarks@anl.gov>
2025-06-03 13:59:26 -0500

    add R macro to db's, move start_caqtdm scripts

--- iocBoot/iocURModule/st.cmd
+++ iocBoot/iocURModule/st.cmd
@@ -7,11 +7,15 @@ iocURModuleLinux_registerRecordDeviceDriver(pdbbase)
 epicsEnvSet("IOCSH_PS1", "$(IOC)>")
 epicsEnvSet("PREFIX", "URModule:")
 
-# Instantiate the client with a name
+# Instantiate HTTP client and load some requests
 AsynHttpClientConfig("client1")
+dbLoadTemplate("requests.substitutions", "P=$(PREFIX),PORT=client1")
 
-# Example: Control robot arm with REST API
-iocshLoad("ur_module.iocsh", "PORT=client1")
+epicsEnvSet("LUA_SCRIPT_PATH", "lua/")
+dbLoadRecords("$(TOP)/URModuleApp/Db/set_movement_params.db", "P=$(PREFIX),R=UR3:,RQ1=Req1:,RQ2=Req2:,HOST=localhost:3030")
+dbLoadRecords("$(TOP)/URModuleApp/Db/toggle_gripper.db", "P=$(PREFIX),R=UR3:,RQ1=Req3:,RQ2=Req4:,HOST=localhost:3030")
+dbLoadRecords("$(TOP)/URModuleApp/Db/gripper_pick.db", "P=$(PREFIX),R=UR3:,RQ1=Req5:,RQ2=Req6:,HOST=localhost:3030")
+dbLoadRecords("$(TOP)/URModuleApp/Db/gripper_place.db", "P=$(PREFIX),R=UR3:,RQ1=Req7:,RQ2=Req8:,HOST=localhost:3030")
 
 ###############################################################################
 iocInit
